3adb59e74ebb44445e6a2cdfdce859e342c490ec
蔡新明 <2109922197@qq.com>
2021-02-23 08:46:52 +0800

    添加项目文件。

---
+++ HotelDAL/RoomService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelDAL
+{
+    /// <summary>
+    /// 房间数据处理
+    /// </summary>
+    public class RoomService
+    {
+
+    }
+}
